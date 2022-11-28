fd77d48c667a751e4e5e2f1542a65b514a1a492d
PhoenixZhao1234 <113965038+PhoenixZhao1234@users.noreply.github.com>
2022-11-28 14:07:47 +0800

    Update RuntimeNetLogic1.cs

--- ProjectFiles/NetSolution/RuntimeNetLogic1.cs
+++ ProjectFiles/NetSolution/RuntimeNetLogic1.cs
@@ -2,13 +2,17 @@
 using System;
 using UAManagedCore;
 using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.UI;
 using FTOptix.Retentivity;
 using FTOptix.NativeUI;
-using FTOptix.UI;
-using FTOptix.HMIProject;
 using FTOptix.Core;
 using FTOptix.CoreBase;
 using FTOptix.NetLogic;
+using FTOptix.Alarm;
+using FTOptix.EventLogger;
+using FTOptix.Store;
+using FTOptix.SQLiteStore;
 using FTOptix.OPCUAServer;
 #endregion
 
@@ -23,4 +27,15 @@ public class RuntimeNetLogic1 : BaseNetLogic
     {
         // Insert code to be executed when the user-defined logic is stopped
     }
+
+    [ExportMethod]
+    public void AddVariable()
+    {
+        var myVar = InformationModel.MakeVariable("Variable" + counter, OpcUa.DataTypes.Int32, new uint[0]);
+        var modelFolder = Project.Current.Get("Model");
+        modelFolder.Add(myVar);
+        counter += 1;
+    }
+
+    private int counter = 3;
 }
